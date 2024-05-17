5007ce2d476082a721e1f29358ea7d2be46b7a4c
Nguyen Hoang Kien <114424432+neikhn@users.noreply.github.com>
2024-05-17 22:23:20 +0700

    fix image file upload size
    all file size of a single product must be less then 2MB

--- ISD_backend-main/src/index.js
+++ ISD_backend-main/src/index.js
@@ -11,6 +11,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
+<<<<<<< Updated upstream
 app.use(
   cors({
     origin: "http://localhost:3000", // Địa chỉ của frontend
@@ -20,6 +21,12 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(bodyParser.json());
+=======
+app.use(cors())
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+// app.use(express.json())
+>>>>>>> Stashed changes
 app.use(cookieParser());
 
 routes(app);
